780f0adc9049e80b151e7d5557ab504dcbc00ca5
Sunshuaiqi <sunshuaiqi@gmail.com>
2015-11-14 22:58:56 +0800

    _

---
+++ SwiftButton/BCSwiftButton.swift
@@ -0,0 +1,202 @@
+//
+//  BCSwiftButton.swift
+//  BCSwiftButton
+//
+//  Created by Sunshuaiqi on 11/13/15.
+//  Copyright © 2015 com.sunshuaiqi. All rights reserved.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+import UIKit
+
+
+@objc protocol BCSwiftButtonDelegate{
+    optional func activatedButton(number : Int)
+}
+
+class BCSwiftButton: UIView {
+
+    // tools
+    var delegate:BCSwiftButtonDelegate!
+    var timer : NSTimer!
+    var isValidTrigger = false
+    var BCSwiftButtonHasShown = false
+    var touchLocation : CGPoint!
+    var size : Double! // button's size
+    var buttonsArray : [UIButton] // embarks all buttons required in this operation
+    var distance : Double // the center-to-center distance between generated buttons and touch point
+    var spreadDuration  = 0.35
+    var buttonActivated = 0
+    var buttonActivationSequence = 0
+    var buttonDeactivationSequence = 0
+    
+    init(buttons:[UIButton], distanceFromCenter:Double, buttonSize:Double, frame: CGRect) {
+        
+        buttonsArray = buttons
+        distance = distanceFromCenter
+        size = buttonSize
+        for var i = 1; i <= buttonsArray.count; i++ {
+            buttonsArray[i-1].tag = i
+            buttonDeactivationSequence += i
+        }
+        print(buttonDeactivationSequence)
+       super.init(frame: frame)
+        self.backgroundColor = UIColor ( red: 1.0, green: 0.856, blue: 0.7153, alpha: 1.0)
+        
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        // make isValidTrigger false
+        isValidTrigger = false
+        buttonActivated = 0
+        // get the touch location
+        for touch in touches{
+            touchLocation = touch.locationInView(touch.view)
+        }
+        timer = NSTimer.scheduledTimerWithTimeInterval(0.6, target: self, selector: "timeDue", userInfo: nil, repeats: false)
+    }
+    
+    override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        for touch in touches{
+            let previousLocation = touch.previousLocationInView(touch.view)
+            let currentLocation = touch.locationInView(touch.view)
+            let translationInView = CGPointMake(currentLocation.x - previousLocation.x, currentLocation.y - previousLocation.y)
+            if translationInView.x >= 2 || translationInView.y >= 2{
+                timer.invalidate()
+            }
+            if (BCSwiftButtonHasShown && isValidTrigger){
+                for button in buttonsArray{
+                    let buttonRadius = button.frame.size.width / 2.0
+                    if (sqrt(pow(button.center.x - currentLocation.x, 2) + pow(button.center.y - currentLocation.y, 2)) <= buttonRadius){
+                        buttonActivationSequence = button.tag+1
+                        buttonActivated = button.tag
+                    }else{
+                        buttonActivationSequence += button.tag+1
+                    }
+                }
+            }
+        }
+    }
+    
+    override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        
+        timer.invalidate()
+        if buttonActivationSequence > buttonDeactivationSequence + buttonsArray.count && BCSwiftButtonHasShown || buttonActivationSequence == 0{
+            buttonDismissAnimation()
+        }else{
+            DidFinishSelection()
+        }
+        
+    }
+    
+    func timeDue(){
+        isValidTrigger = true
+        generateButtons()
+    }
+    
+    func generateButtons(){
+        
+        for button in buttonsArray{
+            button.center = touchLocation
+            button.bounds = CGRect(origin: CGPointZero, size: CGSize(width: size, height: size))
+            button.layer.cornerRadius = CGFloat(size) * 0.5
+            button.clipsToBounds = true
+            button.alpha = 0.0
+            self.addSubview(button)
+        }
+        BCSwiftButtonHasShown = true
+        buttonSpreadOutAnimation()
+    }
+    
+    func buttonSpreadOutAnimation(){
+        let angle : Double = 360.0 / Double(buttonsArray.count)
+        for button in buttonsArray{
+
+            UIView.animateWithDuration(spreadDuration) { () -> Void in
+               button.center = CGPoint(
+                    x: Double(self.touchLocation.x) + self.distance * cos(angle * Double(button.tag+1) / 180.0 * M_PI),
+                    y: Double(self.touchLocation.y) + self.distance * sin(angle * Double(button.tag+1) / 180.0 * M_PI))
+                button.alpha = 1.0
+            }
+        }
+    }
+    
+    func buttonDismissAnimation(){
+        BCSwiftButtonHasShown = false
+        if (buttonActivated == 0){
+            for button in self.buttonsArray{
+                
+                UIView.animateWithDuration(spreadDuration, animations: { () -> Void in
+                    button.center = self.touchLocation
+                    button.alpha = 0.0
+                    }, completion: { (Bool) -> Void in
+                        button.removeFromSuperview()
+                })
+            }
+        }else{
+            for button in self.buttonsArray{
+                if button.tag != self.buttonActivated{
+                    UIView.animateWithDuration(spreadDuration, animations: { () -> Void in
+                        button.center = self.touchLocation
+                        button.alpha = 0.0
+                        
+                        }, completion: { (Bool) -> Void in
+                            button.removeFromSuperview()
+                    })
+                }
+                else{
+                        UIView.animateWithDuration(self.spreadDuration/3.0, animations: { () -> Void in
+                            button.center = CGPoint(
+                                x: Double(self.touchLocation.x) + self.distance*0.75 * cos(360.0 / Double(self.buttonsArray.count) * Double(button.tag+1) / 180.0 * M_PI),
+                                y: Double(self.touchLocation.y) + self.distance*0.75 * sin(360.0 / Double(self.buttonsArray.count) * Double(button.tag+1) / 180.0 * M_PI))
+                            button.alpha = 0.4
+                            }, completion: { (Bool) -> Void in
+                                UIView.animateWithDuration(self.spreadDuration/3.0, animations: { () -> Void in
+                                    button.alpha = 1.0
+                                    }, completion: { (Bool) -> Void in
+                                        UIView.animateWithDuration(self.spreadDuration/3.0, animations: { () -> Void in
+                                            button.alpha = 0.4
+                                            }, completion: { (Bool) -> Void in
+                                                UIView.animateWithDuration(self.spreadDuration/3.0, animations: { () -> Void in
+                                                    button.alpha = 1.0
+                                                    }, completion: {(Bool) -> Void in
+                                                        button.alpha = 0.0
+                                                        button.removeFromSuperview()
+                                                })
+                                        })
+                                })
+                        })
+                    }
+                }
+        }
+    }
+    
+    func DidFinishSelection(){
+        if (delegate != nil && buttonActivated != 0) {
+            delegate?.activatedButton!(buttonActivated)
+        }
+        buttonDismissAnimation()
+    }
+}
+
+    
---
+++ SwiftButton/ViewController.swift
@@ -0,0 +1,73 @@
+//
+//  ViewController.swift
+//  BCSwiftButton
+//
+//  Created by Sunshuaiqi on 11/13/15.
+//  Copyright © 2015 com.sunshuaiqi. All rights reserved.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+import UIKit
+
+class ViewController: UIViewController,BCSwiftButtonDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let button1 = UIButton(type: .System)
+        button1.setTitle("b1", forState: .Normal)
+        button1.backgroundColor = UIColor.redColor()
+        
+        let button2 = UIButton(type: .System)
+        button2.setTitle("b2", forState: .Normal)
+        button2.backgroundColor = UIColor.yellowColor()
+        
+        let button3 = UIButton(type: .System)
+        button3.setTitle("b3", forState: .Normal)
+        button3.backgroundColor = UIColor.blueColor()
+        
+        let button4 = UIButton(type: .System)
+        button4.setTitle("b4", forState: .Normal)
+        button4.backgroundColor = UIColor.greenColor()
+        
+        let button5 = UIButton(type: .System)
+        button5.setTitle("b5", forState: .Normal)
+        button5.backgroundColor = UIColor.cyanColor()
+        
+        let button6 = UIButton(type: .System)
+        button6.setTitle("b6", forState: .Normal)
+        button6.backgroundColor = UIColor.brownColor()
+        
+        let swiftButton = BCSwiftButton(buttons: [button1,button2,button3,button4,button5,button6], distanceFromCenter: 44.0, buttonSize: 44.0, frame: self.view.frame)
+        
+        swiftButton.delegate = self
+        self.view.addSubview(swiftButton)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func activatedButton(number: Int) {
+        print("button number: \(number) was clicked")
+    }
+
+}
+
