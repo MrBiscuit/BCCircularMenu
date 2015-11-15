ec4040bfe0303aafb70c09b89b033c0490e0ec73
Mr.Biscuit <sunshuaiqi@gmail.com>
2015-11-15 15:29:32 +0800

    Update ViewController.swift

--- BCCircularMenu/ViewController.swift
+++ BCCircularMenu/ViewController.swift
@@ -30,23 +30,29 @@ class ViewController: UIViewController,BCCircularMenuDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let image1 = UIImage(data: NSData(contentsOfURL: NSURL(string: "http://i12.tietuku.com/6a1f1e9c9c92e4af.png")!)!)
+        let image2 = UIImage(data: NSData(contentsOfURL: NSURL(string: "http://i12.tietuku.com/b4904da58b2a91d5.png")!)!)
+        let image3 = UIImage(data: NSData(contentsOfURL: NSURL(string: "http://i12.tietuku.com/6a1f1e9c9c92e4af.png")!)!)
+        let image4 = UIImage(data: NSData(contentsOfURL: NSURL(string: "http://i12.tietuku.com/8bbd419f38042e2b.png")!)!)
+        let image5 = UIImage(data: NSData(contentsOfURL: NSURL(string: "http://i12.tietuku.com/8c0fe9bd2f1a7481.png")!)!)
+        
         let button1 = UIButton(type: .Custom)
-        button1.setImage(UIImage(named: "check"), forState: .Normal)
+        button1.setImage(image1, forState: .Normal)
         
         let button2 = UIButton(type: .Custom)
-        button2.setImage(UIImage(named: "x"), forState: .Normal)
+        button2.setImage(image2, forState: .Normal)
         
         let button3 = UIButton(type: .Custom)
-        button3.setImage(UIImage(named: "sms"), forState: .Normal)
+        button3.setImage(image3, forState: .Normal)
         
         let button4 = UIButton(type: .Custom)
-        button4.setImage(UIImage(named: "@"), forState: .Normal)
+        button4.setImage(image4, forState: .Normal)
         
         let button5 = UIButton(type: .Custom)
-        button5.setImage(UIImage(named: "back"), forState: .Normal)
+        button5.setImage(image5, forState: .Normal)
         
         let CircularMenu = BCCircularMenu(buttons: [button1,button2,button3, button4, button5], distanceFromCenter: 90.0, buttonSize: 90.0, frame: self.view.frame)
-        
+        CircularMenu.backgroundColor = UIColor ( red: 1.0, green: 0.8145, blue: 0.4419, alpha: 1.0 )
         CircularMenu.delegate = self
         self.view.addSubview(CircularMenu)
     }
